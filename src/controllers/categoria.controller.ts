54e4e19aaff561c3ab17ce436eb4d7d16817fdeb
Jeferson Arango L <jeferarlo2@gmail.com>
2021-11-25 11:55:08 -0500

    se protege con la estrategia admin todos los metodos de categoría

--- src/controllers/categoria.controller.ts
+++ src/controllers/categoria.controller.ts
@@ -1,30 +1,26 @@
+import {authenticate} from '@loopback/authentication';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Categoria} from '../models';
 import {CategoriaRepository} from '../repositories';
 
+@authenticate("admin")
 export class CategoriaController {
   constructor(
     @repository(CategoriaRepository)
-    public categoriaRepository : CategoriaRepository,
-  ) {}
+    public categoriaRepository: CategoriaRepository,
+  ) { }
 
   @post('/categorias')
   @response(200, {
